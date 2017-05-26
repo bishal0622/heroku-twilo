825b92954a1c1121c45c3273631b26537efdfbe9
bishal <monster_bishal@live.com>
2017-05-26 16:51:01 +0545

    few changes

--- checkSyntax.js
+++ checkSyntax.js
@@ -1,9 +1,9 @@
 module.exports = function(syntaxTest) {
     var anymatch = require('anymatch');
     //pattterns matching using regular expressions
-    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt)[ \t][0-9]{4}$/];
+    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt)$/];
     return anymatch(matchers, syntaxTest, true);
 }
 
 // [ \t][0-9]{4}
-// [ \t][\w.-]{32}
+// [ \t][\w.-]{32}s
