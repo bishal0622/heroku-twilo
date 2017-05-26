0f29b4007b1893c01a24cbe94760d216a766ff65
bishal <monster_bishal@live.com>
2017-05-26 17:04:50 +0545

    asc

--- checkSyntax.js
+++ checkSyntax.js
@@ -1,7 +1,7 @@
 module.exports = function(syntaxTest) {
     var anymatch = require('anymatch');
     //pattterns matching using regular expressions
-    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt|Bt|bT)[ \t][0-9]{2,6}[\t][\w.-]{5,32}$/];
+    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt|Bt|bT)[ \t][0-9]{2,6}[\t][\w]{5,32}$/];
     return anymatch(matchers, syntaxTest, true);
 }
 
