f7ebce222a80c1fb76b6f349ddf25ac07925bdc0
bishal <monster_bishal@live.com>
2017-05-26 16:44:23 +0545

    changes to api

--- checkSyntax.js
+++ checkSyntax.js
@@ -1,8 +1,9 @@
 module.exports = function(syntaxTest) {
     var anymatch = require('anymatch');
     //pattterns matching using regular expressions
-    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt)[ \t][0-9]{4}[ \t][\w.-]{32}$/];
+    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt)[ \t][0-9]{4}$/];
     return anymatch(matchers, syntaxTest, true);
 }
 
 // [ \t][0-9]{4}
+// [ \t][\w.-]{32}
