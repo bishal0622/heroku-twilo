8f9009317f1c79758d760ddc6b9a7bb7fe67af03
bishal <monster_bishal@live.com>
2017-05-26 17:11:08 +0545

    acd

--- checkSyntax.js
+++ checkSyntax.js
@@ -1,9 +1,9 @@
 module.exports = function(syntaxTest) {
     var anymatch = require('anymatch');
     //pattterns matching using regular expressions
-    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt|Bt|bT)[ \t][0-9]{2,6}[\t][\W]{5,32}$/];
+    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt|Bt|bT)[ \t]^[a-zA-Z0-9][\w\.]+[a-zA-Z0-9]$/];
     return anymatch(matchers, syntaxTest, true);
 }
 
 // [ \t][0-9]{4}
-// [ \t][\w.-]{32}s
+// [ \t][\w.-]{32}
