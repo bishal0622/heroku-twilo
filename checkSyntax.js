b111d265edaf534d8a827f95a55c7033fdff1708
bishal <monster_bishal@live.com>
2017-05-27 07:50:25 +0545

    changed to pay

--- checkSyntax.js
+++ checkSyntax.js
@@ -1,7 +1,7 @@
 module.exports = function(syntaxTest) {
     var anymatch = require('anymatch');
     //pattterns matching using regular expressions
-    var matchers = [/\b(BI|bi|Bi)$/, /\b(BT|bt|Bt|bT)[ \t][0-9]{2,6}[\t][a-z]|[1-9]|[A-Z]|[-]|[_]|[.]$/];
+    var matchers = [/\b(BI|bi|Bi)$/, /\b(Pay|pay)[ \t][0-9]{2,6}[\t][a-z]|[1-9]|[A-Z]|[-]|[_]|[.]$/];
     return anymatch(matchers, syntaxTest, true);
 }
 
