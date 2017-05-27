e09975cc2fb52c47dd441dae884f3b893cfd17cf
bishal <monster_bishal@live.com>
2017-05-27 07:58:15 +0545

    pay added

--- checkSyntax.js
+++ checkSyntax.js
@@ -1,6 +1,7 @@
 module.exports = function(syntaxTest) {
     var anymatch = require('anymatch');
     //pattterns matching using regular expressions
+    //pay added
     var matchers = [/\b(BI|bi|Bi)$/, /\b(Pay|pay)[ \t][0-9]{2,6}[\t][a-z]|[1-9]|[A-Z]|[-]|[_]|[.]$/];
     return anymatch(matchers, syntaxTest, true);
 }
